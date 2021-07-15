9f9b3b712547a34232cab5182c97e1a51510e190
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-15 21:00:23 +0200

    feat(bin): add start-server script

---
+++ src/bin/start-server.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata';
+
+import { Script } from '../common/modules/Script';
+import { IServer } from '../server/interfaces/IServer';
+import { serverInjectionTypes } from '../server/inversify/serverInjectionTypes';
+
+class StartServer extends Script {
+  private readonly server: IServer;
+  private readonly PROCESS_SIGNAL_SIGINT: string = 'SIGINT';
+  private readonly PROCESS_SIGNAL_SIGTERM: string = 'SIGTERM';
+  private readonly PROCESS_EXIT_CODE_ERROR: number = -1;
+
+  constructor() {
+    super();
+
+    this.server = this.inversifyContainer.get(
+      serverInjectionTypes.ExpressServer,
+    );
+  }
+
+  public async run(): Promise<void> {
+    await this.server.start();
+
+    process.once(
+      this.PROCESS_SIGNAL_SIGINT,
+      this.createCloseSignalListener.bind(this),
+    );
+
+    process.once(
+      this.PROCESS_SIGNAL_SIGTERM,
+      this.createCloseSignalListener.bind(this),
+    );
+  }
+
+  private createCloseSignalListener(): void {
+    try {
+      void this.server.stop();
+    } catch (error: unknown) {
+      console.error(error);
+
+      process.exit(this.PROCESS_EXIT_CODE_ERROR);
+    }
+  }
+}
+
+void new StartServer().run();
