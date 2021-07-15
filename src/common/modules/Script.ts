90f4f3d405c922a6794994ea75eb534e0a81af2b
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-15 20:58:52 +0200

    feat(common): add Script module

---
+++ src/common/modules/Script.ts
@@ -0,0 +1,16 @@
+import * as inversify from 'inversify';
+
+import { InversifyContainerBuilder } from '../../inversify/InversifyContainerBuilder';
+
+export abstract class Script {
+  protected inversifyContainer: inversify.Container;
+
+  constructor() {
+    const inversifyContainerBuilder: InversifyContainerBuilder =
+      new InversifyContainerBuilder();
+
+    this.inversifyContainer = inversifyContainerBuilder.build();
+  }
+
+  public abstract run(): Promise<void>;
+}
