470990e438755c0f9f15d491fc6cc9ee19234bd9
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-19 00:23:37 +0200

    feat(common): update RequestContext with matching property

--- src/common/models/domain/RequestContext.ts
+++ src/common/models/domain/RequestContext.ts
@@ -1,4 +1,5 @@
 import { CriteriaCompliance } from '../../../criteria-compliance/models/domain/CriteriaCompliance';
+import { Matching } from '../../../matching/models/domain/Matching';
 import { SelectedCriteria } from '../../../selected-criteria/models/domain/SelectedCriteria';
 import { User } from '../../../user/models/domain/User';
 import { ApiVersion } from './ApiVersion';
@@ -6,6 +7,7 @@ import { ApiVersion } from './ApiVersion';
 export interface RequestContext {
   apiVersion?: ApiVersion;
   criteriaCompliance?: CriteriaCompliance;
+  matching?: Matching;
   selectedCriteria?: SelectedCriteria;
   user?: User;
 }
