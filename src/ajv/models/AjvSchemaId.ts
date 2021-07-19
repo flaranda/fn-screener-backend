7d8b0b162213cfd7f9cfcf3260bf8b4594ed9c41
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-19 17:19:45 +0200

    fat(ajv): add MatchingApiV1UpdateQuerySchema json schema

--- src/ajv/models/AjvSchemaId.ts
+++ src/ajv/models/AjvSchemaId.ts
@@ -1,4 +1,5 @@
 export enum AjvSchemaId {
   CriteriaComplianceApiV1UpdateQuery = 'CriteriaComplianceApiV1UpdateQuery',
+  MatchingApiV1UpdateQuery = 'MatchingApiV1UpdateQuery',
   SelectedCriteriaApiV1UpdateQuery = 'SelectedCriteriaApiV1UpdateQuery',
 }
