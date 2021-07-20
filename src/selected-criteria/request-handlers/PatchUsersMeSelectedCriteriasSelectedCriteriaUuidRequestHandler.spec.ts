670d0b3fb9147a29d30d7744b98abe211e7db139
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-20 19:51:35 +0200

    feat(selected-criteria): update PatchUsersMeSelectedCriteriasSelectedCriteriaUuidExpressRequestHandler to use PatchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer

---
+++ src/selected-criteria/request-handlers/PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler.spec.ts
@@ -0,0 +1,172 @@
+import 'reflect-metadata';
+
+jest.mock('express');
+
+import express from 'express';
+
+import { IInteractor } from '../../common/interfaces/IInteractor';
+import { ITransformer } from '../../common/interfaces/ITransformer';
+import { ApiVersion } from '../../common/models/domain/ApiVersion';
+import { requestContextSymbol } from '../../common/models/domain/requestContextSymbol';
+import { RequestWithContext } from '../../server/models/RequestWithContext';
+import { UserFixtures } from '../../user/fixtures/domain/UserFixtures';
+import { SelectedCriteriaApiV1Fixtures } from '../fixtures/api/v1/SelectedCriteriaApiV1Fixtures';
+import { SelectedCriteriaFixtures } from '../fixtures/domain/SelectedCriteriaFixtures';
+import { SelectedCriteriaUpdateQueryFixtures } from '../fixtures/domain/SelectedCriteriaUpdateQueryFixtures';
+import { SelectedCriteriaApiV1 } from '../models/api/v1/SelectedCriteriaApiV1';
+import { SelectedCriteria } from '../models/domain/SelectedCriteria';
+import { SelectedCriteriaUpdateQuery } from '../models/domain/SelectedCriteriaUpdateQuery';
+import { PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler } from './PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler';
+
+describe('PatchUsersMeSelectedCriteriasSelectedCriteriaUuidExpressRequestHandler', () => {
+  let patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer: jest.Mocked<
+    ITransformer<RequestWithContext, SelectedCriteriaUpdateQuery>
+  >;
+  let updateSelectedCriteriaInteractor: jest.Mocked<
+    IInteractor<SelectedCriteriaUpdateQuery, SelectedCriteria>
+  >;
+  let matchingToSelectedCriteriaApiV1Transformer: jest.Mocked<
+    ITransformer<SelectedCriteria, SelectedCriteriaApiV1>
+  >;
+
+  let patchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler: PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler;
+
+  beforeAll(() => {
+    let expressRouterMockHandler: express.RequestHandler | undefined =
+      undefined;
+
+    const expressRouterMock: jest.Mocked<express.Router> = jest
+      .fn()
+      .mockImplementation(
+        (
+          request: express.Request,
+          response: express.Response,
+          next: express.NextFunction,
+        ) => {
+          if (expressRouterMockHandler) {
+            expressRouterMockHandler(request, response, next);
+          }
+        },
+      ) as Partial<express.Router> as jest.Mocked<express.Router>;
+
+    expressRouterMock.use = jest
+      .fn()
+      .mockImplementation(
+        (handler: express.RequestHandler): express.RequestHandler => {
+          expressRouterMockHandler = handler;
+
+          return expressRouterMock;
+        },
+      );
+
+    (express.Router as jest.Mock).mockReturnValue(expressRouterMock);
+
+    patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer =
+      {
+        transform: jest.fn(),
+      };
+
+    updateSelectedCriteriaInteractor = {
+      interact: jest.fn(),
+    };
+
+    matchingToSelectedCriteriaApiV1Transformer = {
+      transform: jest.fn(),
+    };
+
+    patchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler =
+      new PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler(
+        patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer,
+        updateSelectedCriteriaInteractor,
+        matchingToSelectedCriteriaApiV1Transformer,
+      );
+  });
+
+  describe('.handler()', () => {
+    let expressResponseMock: jest.Mocked<express.Response>;
+    let expressNextFunctionMock: jest.Mocked<express.NextFunction>;
+
+    beforeAll(() => {
+      expressResponseMock = {
+        json: jest.fn(),
+      } as Partial<express.Response> as jest.Mocked<express.Response>;
+
+      expressNextFunctionMock = jest.fn();
+
+      patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer.transform.mockResolvedValue(
+        SelectedCriteriaUpdateQueryFixtures.withMandatory,
+      );
+
+      updateSelectedCriteriaInteractor.interact.mockResolvedValue(
+        SelectedCriteriaFixtures.withMandatory,
+      );
+
+      matchingToSelectedCriteriaApiV1Transformer.transform.mockResolvedValue(
+        SelectedCriteriaApiV1Fixtures.withMandatory,
+      );
+    });
+
+    describe('having an ExpressRequest with ApiVersion.v1 and User', () => {
+      let expressRequestMock: RequestWithContext;
+
+      beforeAll(() => {
+        expressRequestMock = {
+          [requestContextSymbol]: {
+            apiVersion: ApiVersion.v1,
+            user: UserFixtures.withMandatory,
+          },
+        } as RequestWithContext;
+      });
+
+      describe('when called', () => {
+        beforeAll(async () => {
+          await (patchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler.handler(
+            expressRequestMock,
+            expressResponseMock,
+            expressNextFunctionMock,
+          ) as unknown as Promise<void>);
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call PatchV1SelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer.trasnform()', () => {
+          expect(
+            patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer.transform,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer.transform,
+          ).toHaveBeenCalledWith(expressRequestMock);
+        });
+
+        it('should call UpdateSelectedCriteriaInteractor.interact()', () => {
+          expect(
+            updateSelectedCriteriaInteractor.interact,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            updateSelectedCriteriaInteractor.interact,
+          ).toHaveBeenCalledWith(
+            SelectedCriteriaUpdateQueryFixtures.withMandatory,
+          );
+        });
+
+        it('should call SelectedCriteriaToSelectedCriteriaApiV1Transformer.transform()', () => {
+          expect(
+            matchingToSelectedCriteriaApiV1Transformer.transform,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            matchingToSelectedCriteriaApiV1Transformer.transform,
+          ).toHaveBeenCalledWith(SelectedCriteriaFixtures.withMandatory);
+        });
+
+        it('should call expressResponseMock.json()', () => {
+          expect(expressResponseMock.json).toHaveBeenCalledTimes(1);
+          expect(expressResponseMock.json).toHaveBeenCalledWith(
+            SelectedCriteriaApiV1Fixtures.withMandatory,
+          );
+        });
+      });
+    });
+  });
+});
--- src/selected-criteria/request-handlers/PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler.ts
+++ src/selected-criteria/request-handlers/PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler.ts
@@ -1,18 +1,12 @@
 import express from 'express';
 import * as inversify from 'inversify';
 
-import { getRequestContext } from '../../common/helpers/getRequestContext';
-import { hasValue } from '../../common/helpers/hasValue';
 import { IInteractor } from '../../common/interfaces/IInteractor';
-import { IRequestParser } from '../../common/interfaces/IRequestParser';
 import { ITransformer } from '../../common/interfaces/ITransformer';
-import { RequestContext } from '../../common/models/domain/RequestContext';
 import { RequestWithContext } from '../../server/models/RequestWithContext';
 import { ApiExpressRequestHandler } from '../../server/modules/ApiExpressRequestHandler';
 import { selectedCriteriaInjectionTypes } from '../inversify/selectedCriteriaInjectionTypes';
 import { SelectedCriteriaApiV1 } from '../models/api/v1/SelectedCriteriaApiV1';
-import { selectedCriteriaApiV1ImportanceToSelectedCriteriaImportanceMap } from '../models/api/v1/selectedCriteriaApiV1ImportanceToSelectedCriteriaImportanceMap';
-import { SelectedCriteriaApiV1UpdateQuery } from '../models/api/v1/SelectedCriteriaApiV1UpdateQuery';
 import { SelectedCriteria } from '../models/domain/SelectedCriteria';
 import { SelectedCriteriaUpdateQuery } from '../models/domain/SelectedCriteriaUpdateQuery';
 
@@ -20,9 +14,12 @@ import { SelectedCriteriaUpdateQuery } from '../models/domain/SelectedCriteriaUp
 export class PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler extends ApiExpressRequestHandler {
   constructor(
     @inversify.inject(
-      selectedCriteriaInjectionTypes.PatchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestParser,
+      selectedCriteriaInjectionTypes.PatchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer,
     )
-    private readonly patchV1SelectedCriteriasRequestParser: IRequestParser<SelectedCriteriaApiV1UpdateQuery>,
+    private readonly patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer: ITransformer<
+      RequestWithContext,
+      SelectedCriteriaUpdateQuery
+    >,
     @inversify.inject(
       selectedCriteriaInjectionTypes.UpdateSelectedCriteriaInteractor,
     )
@@ -47,7 +44,9 @@ export class PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler ext
     _next: express.NextFunction,
   ): Promise<void> {
     const selectedCriteriaUpdateQuery: SelectedCriteriaUpdateQuery =
-      await this.transformV1RequestToSelectedCriteriaUpdateQuery(request);
+      await this.patchV1UsersMeSelectedCriteriasSelectedCriteriaUuidRequestToSelectedCriteriaUpdateQueryTransformer.transform(
+        request,
+      );
 
     const selectedCriteria: SelectedCriteria =
       await this.updateSelectedCriteriaInteractor.interact(
@@ -60,27 +59,5 @@ export class PatchUsersMeSelectedCriteriasSelectedCriteriaUuidRequestHandler ext
       );
 
     response.json(selectedCriteriaApiV1);
-  }
-
-  private async transformV1RequestToSelectedCriteriaUpdateQuery(
-    request: RequestWithContext,
-  ): Promise<SelectedCriteriaUpdateQuery> {
-    const selectCriteriaApiV1UpdateQuery: SelectedCriteriaApiV1UpdateQuery =
-      await this.patchV1SelectedCriteriasRequestParser.parse(request);
-
-    const requestContext: RequestContext = getRequestContext(request);
-
-    const selectedCriteriaUpdateQuery: SelectedCriteriaUpdateQuery = {
-      uuid: requestContext.selectedCriteria?.uuid as string,
-    };
-
-    if (hasValue(selectCriteriaApiV1UpdateQuery.importance)) {
-      selectedCriteriaUpdateQuery.importance =
-        selectedCriteriaApiV1ImportanceToSelectedCriteriaImportanceMap[
-          selectCriteriaApiV1UpdateQuery.importance
-        ];
-    }
-
-    return selectedCriteriaUpdateQuery;
   }
 }
