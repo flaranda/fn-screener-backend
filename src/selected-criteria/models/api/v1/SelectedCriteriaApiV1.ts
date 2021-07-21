141581fa0f7c0648b9fd993518459c36a0a0f88f
Francisco Aranda <flarandagarcia@gmail.com>
2021-07-21 13:47:53 +0200

    feat(selected-criteria): update SelectedCriteriaApiV1 to have readonly properties

--- src/selected-criteria/models/api/v1/SelectedCriteriaApiV1.ts
+++ src/selected-criteria/models/api/v1/SelectedCriteriaApiV1.ts
@@ -1,8 +1,8 @@
 import { SelectedCriteriaApiV1Importance } from './SelectedCriteriaApiV1Importance';
 
 export interface SelectedCriteriaApiV1 {
-  criteria_name: string;
-  criteria_uuid: string;
-  importance: SelectedCriteriaApiV1Importance;
-  uuid: string;
+  readonly criteria_name: string;
+  readonly criteria_uuid: string;
+  readonly importance: SelectedCriteriaApiV1Importance;
+  readonly uuid: string;
 }
